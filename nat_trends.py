3ffd9d8106f212da48421b1aac36c9f76168003e
Tara <0xtr@users.noreply.github.com>
2018-09-23 13:11:19 +0100

    Start adding map stuff

--- nat_trends.py
+++ nat_trends.py
@@ -8,16 +8,25 @@ import sqlite3
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
-import ggplot
 
+# import ggplot
 
+date = datetime.date.today()
+day_of_month = date.day
+month = date.month
+year = date.year
+"""
 def remove_non_ascii(text):
     return unidecode(text)
 
 
 dbconn = sqlite3.connect("resources/nat_tracks.db")
 dbcurs = dbconn.cursor()
-dbcurs.execute("CREATE TABLE IF NOT EXISTS nat (day_of_month int, month int, year int, epochtime int, details text)")
+dbcurs.execute("CREATE TABLE IF NOT EXISTS nat ("
+               "direction text, "
+               "day_of_month int, month int, year int, "
+               "epochtime int, "
+               "details text)")
 
 get_new = True
 date = datetime.date.today()
@@ -31,10 +40,8 @@ if len(results) is not 0:
     if len(results) is 5:
         dbcurs.execute("DELETE FROM nat WHERE day_of_month = ? AND month = ? AND year = ?", [day_of_month, month, year])
         print("Clearing partial results collected already for a re-run; deleted " + str(dbcurs.fetchall()) + " rows")
-    elif len(results) is 14:
-        print("Nothing to collect for today; already got 14 tracks")
-        get_new = False
     else:
+        # clarify the maxes we can expect? 10 westbound as of this morning???
         print("Got " + str(len(results)) + " for today")
 
 texts = []
@@ -72,7 +79,8 @@ if get_new is True:
     parser.feed(the_page)
 
     for i in texts:
-        dbcurs.execute("INSERT INTO nat VALUES (?,?,?,?,?)", [day_of_month, month, year, epoch_time, i])
+        direction = "WEST" if "EAST LEVELS NIL" in i else "EAST"
+        dbcurs.execute("INSERT INTO nat VALUES (?, ?,?,?,?,?)", [direction, day_of_month, month, year, epoch_time, i])
         dbconn.commit()
 
 dbconn.close()
@@ -80,7 +88,21 @@ dbconn.close()
 if len(texts) is not 0:
     print("Collected info for " + str(len(texts)) + " NAT tracks")
 
-
 # TODO:
 # text processing
 # mapping
+"""
+
+fig = plt.figure(figsize=(18, 15))
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+m = Basemap(projection='stere', lon_0=-35, lat_0=55, lat_ts=55,
+            width=5000000, height=2000000, resolution='l', area_thresh=10000)
+m.drawcoastlines()
+m.fillcontinents()
+
+m.drawparallels(np.arange(20, 70, 1))
+m.drawmeridians(np.arange(-100, 20, 1))
+
+ax.set_title("North Atlantic Tracks for " + str(day_of_month) + "/" + str(month) + "/" + str(year))
+plt.show()
