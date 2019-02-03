2ec260f47116e735e357dc7867f4224c90483be5
Tara <0xtr@users.noreply.github.com>
2019-02-03 18:51:22 +0000

    Add airprox cluster attempts

--- airprox_clustering.py
+++ airprox_clustering.py
@@ -1,10 +1,14 @@
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib import cm
 from mpl_toolkits.basemap import Basemap
+from sklearn.cluster import DBSCAN
 
 
-def generate_map():
+def generate_map(plotitems):
     fig = plt.figure(figsize=(18, 15))
     ax = fig.add_axes([0.05, 0.05, 0.9, 0.85])
     ax.set_title("Airprox Incidents 2000-2018?")
@@ -14,16 +18,18 @@ def generate_map():
                 urcrnrlat=59, urcrnrlon=5, resolution='l')
     draw_fundamental_map_lines(m)
 
-    latlons = dict(zip(list(cols["Latitude"]), list(cols["Longitude"])))
-    for key, val in latlons.items():
-        plot_lines(m, key, val)
+    colmap = cm.get_cmap('gist_rainbow', 1000)
+
+    for cluster in plotitems:
+        col = colmap(random.uniform(0, 1))
+        for key, val in dict(cluster).items():
+            plot_lines(m, key, val, col)
 
     plt.show()
 
 
 def dms_to_dd(val):
-    degrees = minutes = seconds = 0
-
+    seconds = 0
     if "N" in val or "S" in val:
         degrees = val[0:2]
         minutes = val[2:4]
@@ -38,19 +44,9 @@ def dms_to_dd(val):
     return dd
 
 
-def process_lon_to_float(lon):
-    val = ""
-    if lon[5] is not "E":
-        val += "-"
-
-    lon = lon[2:5]
-    val += lon[0] + "." + lon[1:3]
-    return val
-
-
-def plot_lines(m, lat, lon):
+def plot_lines(m, lat, lon, col):
     x, y = m(lon, lat)
-    m.plot(x, y, marker="+", color="black")
+    m.plot(x, y, marker="o", color=col)
 
 
 def draw_fundamental_map_lines(m):
@@ -62,4 +58,11 @@ def draw_fundamental_map_lines(m):
 data = pd.read_csv("resources/airprox_reports_2000_to_2018.csv")
 cols = data[["Latitude", "Longitude"]]
 cols = cols.applymap(dms_to_dd)
-generate_map()
+cols = cols.values.astype("float32", copy=False)
+
+eps = 11 / 6371.0088
+db = DBSCAN(eps=eps, min_samples=4, algorithm='ball_tree', metric='haversine').fit(np.radians(cols))
+num_clusters = len(set(db.labels_))
+clusters = pd.Series([cols[db.labels_ == n] for n in range(num_clusters)])
+
+generate_map(clusters)
